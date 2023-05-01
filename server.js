3039c2509f95efdbdcf99957f32fcb256b963edd
marco.rojas <marco.rojas@parquedelrecuerdo.cl>
2023-05-01 19:33:03 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!');
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
